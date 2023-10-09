38abc9bf120a83ca28462444f5a5d6c85637e95f
yeni023 <yeeun3011@naver.com>
2023-10-09 15:00:21 +0900

    .h -> plus .c file

--- 2/minitalk/bonus/minitalk_bonus.h
+++ 2/minitalk/bonus/minitalk_bonus.h
@@ -6,7 +6,7 @@
 /*   By: yeeunpar <yeeunpar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 12:18:15 by yeeunpar          #+#    #+#             */
-/*   Updated: 2023/10/09 12:19:07 by yeeunpar         ###   ########.fr       */
+/*   Updated: 2023/10/09 14:59:28 by yeeunpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,14 @@
 # include <unistd.h>
 # include <signal.h>
 # include "../src/libft/libft.h"
+
+//client_bonus.c
+void	send_error_message(void);
+void	check_message(int signal);
+void	send_bit(pid_t pid, char input);
+void	send_string(pid_t pid, char *input);
+//server_bonus.c
+void	check_handler(int signal, siginfo_t *info, void *s);
+void	print_pid(pid_t pid);
 
 #endif
--- 2/minitalk/mandatory/minitalk.h
+++ 2/minitalk/mandatory/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: yeeunpar <yeeunpar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 11:36:33 by yeeunpar          #+#    #+#             */
-/*   Updated: 2023/10/09 11:37:17 by yeeunpar         ###   ########.fr       */
+/*   Updated: 2023/10/09 14:57:58 by yeeunpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,13 @@
 # include <unistd.h>
 # include <signal.h>
 # include "../src/libft/libft.h"
+
+// client.c
+void	check_handler(int signal);
+void	print_pid(pid_t pid);
+// server.c
+void	send_error_message(void);
+void	send_bit(pid_t pid, char input);
+void	send_string(pid_t pid, char *input);
 
 #endif
